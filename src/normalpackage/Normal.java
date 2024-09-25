553c26d14e31ae7ed3d1d0166feccdd4024d8094
narendrabasam <inarendra913@gmail.com>
2024-09-25 22:46:20 +0530

    First Commit

---
+++ src/normalpackage/Normal.java
@@ -0,0 +1,12 @@
+package normalpackage;
+
+public class Normal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("start coding");
+		System.out.println(3+3);
+
+	}
+
+}
